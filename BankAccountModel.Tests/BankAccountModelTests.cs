a7d7997ac54fe8a30cb146f7494c5ad5e18aae47
David Newman <David.Newman@mhr.co.uk>
2018-08-15 10:22:05 +0100

    Added additional integration tests

--- BankAccountModel.Tests/BankAccountModelTests.cs
+++ BankAccountModel.Tests/BankAccountModelTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using BankAccountModel.Domain;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NHibernate;
@@ -9,18 +10,68 @@ namespace BankAccountModel.Tests
     public class BankAccountModelTests
     {
         [TestMethod]
-        public void CanUpdatePrimaryBankAccount()
+        public void CanCreatePortfolioWithoutBankAccount()
         {
-            Guid bankAccountPortfolioId;
-
+            BankAccountPortfolio portfolio;
             ISession session = SessionFactory.Create();
             using (var tran = session.BeginTransaction())
             {
-                BankAccountPortfolio portfolio = new BankAccountPortfolio();
+                portfolio = new BankAccountPortfolio();
+
+                session.Save(portfolio);
+
+                tran.Commit();
+            }
+
+            using (var tran = session.BeginTransaction())
+            {
+                BankAccountPortfolio bankAccountPortfolio = session.Get<BankAccountPortfolio>(portfolio.Id);
+                Assert.AreEqual(0, bankAccountPortfolio.BankAccountSplits.Count());
+                tran.Commit();
+            }
+        }
+
+        [TestMethod]
+        public void CanCreatePortfolioThenAddBankAccountAtLaterDate()
+        {
+            BankAccountPortfolio portfolio;
+            ISession session = SessionFactory.Create();
+            using (var tran = session.BeginTransaction())
+            {
+                portfolio = new BankAccountPortfolio();
+
+                session.Save(portfolio);
+
+                tran.Commit();
+            }
+
+            using (var tran = session.BeginTransaction())
+            {
+                BankAccountPortfolio bankAccountPortfolio = session.Get<BankAccountPortfolio>(portfolio.Id);
+                bankAccountPortfolio.AddBankAccount("First-111111111");
+
+                tran.Commit();
+            }
+
+            using (var tran = session.BeginTransaction())
+            {
+                BankAccountPortfolio bankAccountPortfolio = session.Get<BankAccountPortfolio>(portfolio.Id);
+                Assert.AreEqual(1, bankAccountPortfolio.BankAccountSplits.Count());
+                tran.Commit();
+            }
+        }
+
+        [TestMethod]
+        public void CanUpdatePrimaryBankAccount()
+        {
+            BankAccountPortfolio portfolio;
+            ISession session = SessionFactory.Create();
+            using (var tran = session.BeginTransaction())
+            {
+                portfolio = new BankAccountPortfolio();
                 BankAccount firstBankAccount = portfolio.AddBankAccount("First-0123456789");
 
                 session.Save(portfolio);
-                bankAccountPortfolioId = portfolio.Id;
 
                 tran.Commit();
 
@@ -30,7 +81,7 @@ namespace BankAccountModel.Tests
             BankAccount secondBankAccount;
             using (var tran = session.BeginTransaction())
             {
-                BankAccountPortfolio bankAccountPortfolio = session.Get<BankAccountPortfolio>(bankAccountPortfolioId);
+                BankAccountPortfolio bankAccountPortfolio = session.Get<BankAccountPortfolio>(portfolio.Id);
                 secondBankAccount = bankAccountPortfolio.AddBankAccount("Second-9876543210");
 
                 tran.Commit();
@@ -38,7 +89,7 @@ namespace BankAccountModel.Tests
 
             using (var tran = session.BeginTransaction())
             {
-                BankAccountPortfolio bankAccountPortfolio = session.Get<BankAccountPortfolio>(bankAccountPortfolioId);
+                BankAccountPortfolio bankAccountPortfolio = session.Get<BankAccountPortfolio>(portfolio.Id);
                 bankAccountPortfolio.ChangePrimaryBankAccount(secondBankAccount.Id);
 
                 tran.Commit();
@@ -46,7 +97,7 @@ namespace BankAccountModel.Tests
 
             using (var tran = session.BeginTransaction())
             {
-                BankAccountPortfolio bankAccountPortfolio = session.Get<BankAccountPortfolio>(bankAccountPortfolioId);
+                BankAccountPortfolio bankAccountPortfolio = session.Get<BankAccountPortfolio>(portfolio.Id);
                 Assert.AreEqual(bankAccountPortfolio.PrimaryBankAccountSplit.BankAccount, secondBankAccount);
                 tran.Commit();
             }
